6a594f6bab65ad3f915546045f609ad167b5667a
Stojan <mmrsucks@hotmail.com>
2022-06-13 20:44:23 +0200

    Initial commit

---
+++ src/testt/IAnimals.java
@@ -0,0 +1,4 @@
+package testt;
+
+public interface IAnimals {
+}
---
+++ src/testt/NewClass.java
@@ -0,0 +1,21 @@
+package testt;
+
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+
+public class NewClass {
+
+    public static class FirstTest {
+
+        public static void main(String[] args) {
+
+            System.out.println("Hello World!!");
+            sayHello("Neda");
+        }
+
+        private int age = 4;
+    }
+    public static void sayHello(String name){
+        System.out.println( "Hello "  +  name );
+    }
+
+}
