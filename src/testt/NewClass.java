b4f156ea5ac72ccc76024c4da057eff1f081fe0b
Stojan <mmrsucks@hotmail.com>
2022-06-14 20:57:43 +0200

    Revert "new commit" Revert "new commit" 		` sdis reverts commit 237d8f5cba8e62ad952b09d42196c707da3efda7.

--- src/testt/NewClass.java
+++ src/testt/NewClass.java
@@ -19,8 +19,6 @@ public class NewClass {
     public static void sayHello(String name){
         System.out.println( "Hello "  +  name );
 
-        System.out.println("4414");
-
         System.out.println("555");
     }
 
