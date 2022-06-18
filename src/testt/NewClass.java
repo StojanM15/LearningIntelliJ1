4a678e944ceac7270b789d58b5c2b5486a159487
Stojan <mmrsucks@hotmail.com>
2022-06-18 16:47:48 +0200

    Revert "new"
    This reverts commit 0939e10de5d2a754198030fd3a38546e9ace315c.

--- src/testt/NewClass.java
+++ src/testt/NewClass.java
@@ -17,9 +17,9 @@ public class NewClass {
 
     }
     public static void sayHello(String name){
-        System.out.println( "Hello "  +  name )
+        System.out.println( "Hello "  +  name );
 
-        System.out.println("new name");       
+        System.out.println("55");
 
         System.out.println("goodbye");
 
